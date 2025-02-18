48dd9532c24fa935179fff5d0fa8407f3c951b43
Alim4ik322 <assanovali23@gmail.com>
2025-02-18 12:03:34 +0300

    11

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,19 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 function App() {
-	return (
-  <div>
-     <ul>
-		<li>text1</li>
-		<li>text2</li>
-		<li>text3</li>
-	</ul>
-	<ul>
-		<li>text1</li>
-		<li>text2</li>
-		<li>text3</li>
-	</ul>
-  </div>
-  );
+	return <input />;
 }
 export default App;
