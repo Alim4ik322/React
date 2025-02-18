0b17299a5a1dd3195c8dc458cbe432ffb14bd708
Alim4ik322 <assanovali23@gmail.com>
2025-02-18 11:41:17 +0300

    7

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+function App() {
+  /*7*/
+  return <div>
+		text
+    <p>
+      abzac 1
+    </p>
+    <p>
+      abzac 2
+    </p>
+    <p>
+      abzac 3
+    </p>
+    <p id='eee'>
+      abzac 3
+    </p>
+	</div>;
+  /*8*/
+}
+
+export default App;
