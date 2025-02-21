eb4c5f6289588b457bfa22d402cd6f7ba68edd93
Alim4ik322 <assanovali23@gmail.com>
2025-02-21 18:32:38 +0300

    13

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 function App() {
-	return <input />;
+  const str1 = 'text1';
+  const str2 = 'text2'; 
+  return (
+    <div>
+      <p>{str1}</p>
+      <p>{str2}</p>
+    </div>
+  );
 }
 export default App;
