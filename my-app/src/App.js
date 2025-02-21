447af407ba5e680d60ed3b49092c060abf153b08
Alim4ik322 <assanovali23@gmail.com>
2025-02-21 20:52:05 +0300

    14

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 function App() {
-  const str1 = 'text1';
-  const str2 = 'text2'; 
-  return (
-    <div>
-      <p>{str1}</p>
-      <p>{str2}</p>
-    </div>
-  );
+	const name = 'user';
+	const age  = '30';
+	return <div>
+		name: {name}
+		age:  {age}
+	</div>;
 }
 export default App;
