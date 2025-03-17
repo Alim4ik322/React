ab414e62a3b05884c9f3cc29fbdb77c96a646129
Alim4ik322 <assanovali23@gmail.com>
2025-03-17 12:39:26 +0300

    26

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,18 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 function App() {
-	const num1 = 3;
-	const num2 = 2;
-	const name = 'john';
-	const surname = 'smit';
-	const num = 4;
+	let text;
+	const isAdult = true;
+	
+	if (isAdult) {
+		text = <p>Вы совершеннолетний</p>;
+	}
+	else {
+		text = <p>Вы несовершеннолетний.</p>;
+	}
+	
 	return <div>
-		<p>Задание 1</p>
-		<p>result: {num1 ** num2}</p>
-		<p>Задание 2</p>
-		<p>result: {name + ' ' + surname}</p>
-		<p>Задание 3</p>
-		<p>result: {Math.sqrt(num)}</p>
+		{text}
 	</div>;
 }
+  
 export default App;
