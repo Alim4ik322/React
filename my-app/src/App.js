e695efa00481e01f642c41c601a675ac1c11edcf
Alim4ik322 <assanovali23@gmail.com>
2025-03-17 20:07:11 +0300

    47

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { nanoid } from 'nanoid';
+import uuid from 'react-uuid';
 function App() {
-	const users = [
-	  { id: 1, name: 'user1', surn: 'surn1', age: 30 },
-	  { id: 2, name: 'user2', surn: 'surn2', age: 31 },
-	  { id: 3, name: 'user3', surn: 'surn3', age: 32 },
-	];
-	const rows = users.map(function (item) {
-	  return (
-		<tr key={item.id}>
-		  <td>{item.name}</td>
-		  <td>{item.surn}</td>
-		  <td>{item.age}</td>
-		</tr>
-	  );
-	});
-	return (
-	  <table>
-		<thead>
-		  <tr>
-			<th>Имя</th>
-			<th>Фамилия</th>
-			<th>Возраст</th>
-		  </tr>
-		</thead>
-		<tbody>{rows}</tbody>
-	  </table>
-	);
+  const id1 = nanoid(); 
+  const id2 = uuid();
+
+  return (
+    <div>
+      <p>Сгенерированный ID (nanoid): {id1}</p>
+	  <p>Сгенерированный UUID (react-uuid): {id2}</p>
+    </div>
+  );
 }
+
 export default App;
   
   
