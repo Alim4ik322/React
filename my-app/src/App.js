728c189ff7014308b2da7bd0a013d4308471ddeb
Alim4ik322 <assanovali23@gmail.com>
2025-03-17 19:31:28 +0300

    42

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 const users = [
-	{name: 'user1', surn: 'surn1', age: 30}, 
-	{name: 'user2', surn: 'surn2', age: 31}, 
-	{name: 'user3', surn: 'surn3', age: 32}, 
+	{id: 1, name: 'user1', surn: 'surn1', age: 30}, 
+	{id: 2, name: 'user2', surn: 'surn2', age: 31}, 
+	{id: 3, name: 'user3', surn: 'surn3', age: 32}, 
 ];
 function App() {
 	const res = users.map(function(item, index) {
-		return <p key={index}>
+		return <p key={item.id}>
 			<span>{item.name}</span>:
 			<span>{item.surn}</span>:
 			<span>{item.age}</span>
