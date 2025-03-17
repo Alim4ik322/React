e27e20d90f44f1b60ca195f461e164886760865b
Alim4ik322 <assanovali23@gmail.com>
2025-03-17 19:09:17 +0300

    40

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,15 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 function App() {
-	const arr = ['a', 'b', 'c', 'd', 'e'];
-	const res = arr.map(function(item) {
-		return <li>{item}</li>;
+	const arr = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
+  
+	const res = arr.map(function(item, index) {
+	  return <p key={index}>{item}</p>;
 	});
-	
-	return <div>
-		{res}
-	</div>;
-}
+  
+	return <div>{res}</div>;
+  }
 export default App;
   
   
