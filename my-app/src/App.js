6d5849438204829bffc740a845001cf23bbb58dd
Alim4ik322 <assanovali23@gmail.com>
2025-03-17 14:40:45 +0300

    37.2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,18 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 function App() {
-	function func1(event) {
-		console.log(event); // объект 
+	function func(arg1, arg2, event) {
+	  console.log(arg1, arg2, event);
 	}
-	function func2(event) {
-		console.log(event.target); // Выводим элемент, на который кликнули
-	  }
-	
-	return <div>
-		    <button onClick={func1}>Вывести объект события</button>
-			<button onClick={func2}>Вывести элемент клика</button>
-	</div>;
-}
+  
+	return (
+	  <div>
+		<button onClick={event => func('eee', 'fff', event)}>act</button>
+	  </div>
+	);
+  }
 export default App;
   
   
