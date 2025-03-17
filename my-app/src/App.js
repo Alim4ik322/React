68562cf57eafd22c96c5433d537cbb4ec7131262
Alim4ik322 <assanovali23@gmail.com>
2025-03-17 13:08:23 +0300

    29

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 function App() {
-	const age = 19;
+	const isAuth = true;
 	
 	return <div>
-		{age >18 ? <p>вы совершеннолетний</p> : <p>вы несовершеннолетний</p>}
+		{isAuth && <p>вы авторизованы</p>}
 	</div>;
-}  
+}
 export default App;
   
   
