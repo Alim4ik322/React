c574ef6c4dae9a118b3b5ab8a1d6cc243e895e26
Alim4ik322 <assanovali23@gmail.com>
2025-03-24 12:07:13 +0300

    52

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import uuid from 'react-uuid';
-const users = [
-    { id: uuid(), name: 'user1', age: 25 },
-    { id: uuid(), name: 'user2', age: 30 },
-    { id: uuid(), name: 'user3', age: 22 },
-    { id: uuid(), name: 'user4', age: 28 },
-    { id: uuid(), name: 'user5', age: 35 },
-  ];
+
 function App() {
-  const res = users.map(function(item, index) {
-    return <p key={index}>
-        <span>{item.id}</span>
-        <span>       </span>
-        <span>{item.name}</span>
-        <span>      </span>
-        <span>{item.surn}</span>
-        <span>       </span>
-        <span>{item.age}</span>
-    </p>;
-});
+const[name, setName]=useState('John')
+const[surname, setSurname]=useState('Johnson')
+const[age, setAge]=useState('28')
 return <div>
-    {res}
-</div>;
+  <span>{name}</span>
+  <span>      </span>
+  <span>{surname}</span>
+  <span>      </span>
+  <span>{age}</span>
+</div>
 }
 
 export default App;
