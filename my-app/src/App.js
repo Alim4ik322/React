95f33ccda0f54cba81fe9b24b292aa97337a8243
Alim4ik322 <assanovali23@gmail.com>
2025-03-24 12:22:21 +0300

    53

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,16 +5,25 @@ import { nanoid } from 'nanoid';
 import uuid from 'react-uuid';
 
 function App() {
-const[name, setName]=useState('John')
-const[surname, setSurname]=useState('Johnson')
-const[age, setAge]=useState('28')
-return <div>
-  <span>{name}</span>
-  <span>      </span>
-  <span>{surname}</span>
-  <span>      </span>
-  <span>{age}</span>
-</div>
+	const[name, setName]=useState('xxxx')
+  const[surname, setSurname]=useState('xxxx')
+  const[age, setAge]=useState('xxxx')
+	
+	return <div>
+    <span>{name}</span>
+    <span>      </span>
+    <span>{surname}</span>
+    <span>      </span>
+    <span>{age}</span>
+    <span>      </span>
+		<button onClick={() => setName('John')}>
+			btn</button> 
+    <span>      </span>
+    <button onClick={() => setSurname('Johnson')}>
+			btn</button> 
+    <button onClick={() => setAge('38')}>
+			btn</button> 
+	</div>;
 }
 
 export default App;
