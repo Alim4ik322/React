0fed821435ff28bb41e41ca750ba1db23650b49f
Alim4ik322 <assanovali23@gmail.com>
2025-05-08 19:29:32 +0300

    58.1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import uuid from 'react-uuid';
+function vozrast(num) {
+	return 2025-num;
+}
+
 function App() {
 	const [value, setValue] = useState(0);
 	
@@ -11,11 +15,9 @@ function App() {
 	}
 	
 	return <div>
-		<input value={value} onChange={handleChange} 
-			/> 
-		<p>{value ** 2}</p>
+		<input value={value} onChange={handleChange} />
+		<p>{vozrast(value)}</p>
 	</div>;
 }
-
 export default App;
   
