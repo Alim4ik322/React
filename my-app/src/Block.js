357f952ad3391b85938044a5c3abcc33fe695f80
Alim4ik322 <assanovali23@gmail.com>
2025-05-13 01:05:00 +0300

    106

--- my-app/src/Block.js
+++ my-app/src/Block.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
 
+// Создание первого стилизованного дива (DIVA)
+const DIVA = styled.div`
+    width: 150px;
+    height: 150px;
+    background-color: yellow;
+    border: 2px solid black;
+`;
+
+// Создание второго стилизованного дива (DIVB) на основе DIVA
+const DIVB = styled(DIVA)`
+    background-color: green;
+    border-width: 3px;
+`;
+
+// Контейнер для размещения компонентов
 const Container = styled.div`
     display: flex;
     gap: 10px;
 `;
 
-const Button = styled.button`
-    padding: 8px 12px;
-    border: none;
-    cursor: pointer;
-
-    background-color: ${({ warn }) => (warn ? 'yellow' : 'green')};
-    color: ${({ warn }) => (warn ? 'red' : 'white')};
-`;
-
+// Основной компонент Block1
 function Block() {
     return (
         <Container>
-            <Button disabled>Первая</Button>
-            <Button warn>Вторая</Button>
-            <Button type="reset">Третья</Button>
+            <DIVA />
+            <DIVB />
+            <DIVA />
         </Container>
     );
 }
