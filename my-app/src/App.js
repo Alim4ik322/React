11ae5896f5deb0bbeca0441c35ad26ea2b84e881
Alim4ik322 <assanovali23@gmail.com>
2025-05-12 18:25:26 +0300

    70

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,21 +6,34 @@ import uuid from 'react-uuid';
 // ❌ Удалено: import { useState } from "react";
 
 function App() {
-	const [text, setText] = useState('Привет, React!');
-	const [isChecked, setIsChecked] = useState(true);
+  const [notes, setNotes] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
-	return (
-		<div>
-			<h3>Инпут с defaultValue:</h3>
-			<input defaultValue={text} />
+  function changeHandler(index, event) {
+    const newNotes = [...notes];
+    newNotes[index] = event.target.value;
+    setNotes(newNotes);
+  }
 
-			<h3>Чекбокс с defaultChecked:</h3>
-			<label>
-				<input type="checkbox" defaultChecked={isChecked} />
-				Чекбокс по умолчанию {isChecked ? 'отмечен' : 'не отмечен'}
-			</label>
-		</div>
-	);
+  function getAverage(arr) {
+    const nums = arr.map(Number).filter(n => !isNaN(n));
+    const sum = nums.reduce((acc, val) => acc + val, 0);
+    return (sum / nums.length).toFixed(2);
+  }
+
+  return (
+    <div>
+      {notes.map((note, index) => (
+        <input
+          key={index}
+          value={note}
+          onChange={event => changeHandler(index, event)}
+          style={{ marginRight: '5px' }}
+        />
+      ))}
+      <p>Среднее арифметическое: {getAverage(notes)}</p>
+    </div>
+  );
 }
+
 
 export default App;
