9dfb305e48a1b253c130ac1cadc60fcbafb992d7
Alim4ik322 <assanovali23@gmail.com>
2025-05-12 19:23:48 +0300

    74.4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,23 +6,39 @@ import uuid from 'react-uuid';
 // ❌ Удалено: import { useState } from "react";
 
 function App() {
-	const [notes] = useState(['a', 'b', 'c']);
+	const [notes, setNotes] = useState(['a', 'b', 'c']);
 	const [input, setInput] = useState('');
+	const [selectedIndex, setSelectedIndex] = useState(null);
 
-	function handleClick(note) {
-		setInput(note);
+	function handleClick(index) {
+		setSelectedIndex(index);
+		setInput(notes[index]);
+	}
+
+	function handleBlur() {
+		if (selectedIndex !== null) {
+			const copy = [...notes];
+			copy[selectedIndex] = input;
+			setNotes(copy);
+			setSelectedIndex(null);
+			setInput('');
+		}
 	}
 
 	return (
 		<div>
 			<ul>
 				{notes.map((note, index) => (
-					<li key={index} onClick={() => handleClick(note)}>
+					<li key={index} onClick={() => handleClick(index)}>
 						{note}
 					</li>
 				))}
 			</ul>
-			<input value={input} onChange={(e) => setInput(e.target.value)} />
+			<input
+				value={input}
+				onChange={(e) => setInput(e.target.value)}
+				onBlur={handleBlur}
+			/>
 		</div>
 	);
 }
