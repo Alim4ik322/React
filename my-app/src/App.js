b9858c7a29ae37de6d37de7650b8da00c0697a3f
Alim4ik322 <assanovali23@gmail.com>
2025-05-13 00:40:36 +0300

    101

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,12 +11,33 @@ import { styles } from './styles'; // Импорт объекта со стил�
 
 function App() {
 	return (
-		<div style={styles.class1}>
-			<p style={styles.class2}>text</p>
-			<p style={styles.class3}>text</p>
-			<p style={styles.class4}>text</p>
+		<div style={{
+			width: '200px',
+			border: '2px solid brown',
+			padding: '10px',
+			textAlign: 'center',
+		}}>
+			<p style={{
+				color: 'orangered',
+				fontWeight: 'bold',
+			}}>
+				text
+			</p>
+			<p style={{
+				fontStyle: 'italic',
+				color: 'brown',
+			}}>
+				text
+			</p>
+			<p style={{
+				backgroundColor: 'orange',
+				fontWeight: 'bold',
+				color: 'white',
+			}}>
+				text
+			</p>
 		</div>
 	);
 }
 
-export default App;
+export default App
