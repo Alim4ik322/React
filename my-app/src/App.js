b51fda318ce333b5ed47a1df67ef77dcc2ed31eb
Alim4ik322 <assanovali23@gmail.com>
2025-05-13 00:22:56 +0300

    98

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,44 +7,14 @@ import User from './User';
 import Employee from './Employee';
 import Users from './Users';
 import { nanoid as id } from 'nanoid';
+import './styles.css';
+
 function App() {
-  const [notes, setNotes] = useState([1, 2, 3, 4, 5]);
-  const [editNum, setEditNum] = useState(null);
-  const [value, setValue] = useState('');
-
-  function startEdit(index) {
-    setEditNum(index);
-    setValue(notes[index]);
-  }
-
-  function changeItem(event) {
-    setValue(event.target.value);
-  }
-
-  function saveItem() {
-    const updated = [...notes];
-    updated[editNum] = value;
-    setNotes(updated);
-    setEditNum(null);
-    setValue('');
-  }
-
   return (
-    <div>
-      {notes.map((note, index) => (
-        <p key={index} onClick={() => startEdit(index)}>
-          {note}
-        </p>
-      ))}
-
-      {editNum !== null && (
-        <>
-          <input value={value} onChange={changeItem} />
-          <button onClick={saveItem}>Сохранить</button>
-        </>
-      )}
+    <div className="wrapper">
+      <button className="btn1">Кнопка 1</button>
+      <button className="btn2">Кнопка 2</button>
     </div>
   );
 }
-
 export default App;
