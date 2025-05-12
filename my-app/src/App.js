aa06cf659c5708e3eb8e9eaad0472f3de48b23e5
Alim4ik322 <assanovali23@gmail.com>
2025-05-13 00:26:31 +0300

    99

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,14 +7,37 @@ import User from './User';
 import Employee from './Employee';
 import Users from './Users';
 import { nanoid as id } from 'nanoid';
-import './styles.css';
 
 function App() {
-  return (
-    <div className="wrapper">
-      <button className="btn1">Кнопка 1</button>
-      <button className="btn2">Кнопка 2</button>
-    </div>
-  );
+	const class1 = {
+		width: '200px',
+		border: '2px solid brown',
+		padding: '10px',
+		textAlign: 'center',
+	};
+
+	const class2 = {
+		color: 'orangered',
+		fontWeight: 'bold',
+	};
+
+	const class3 = {
+		fontStyle: 'italic',
+		color: 'brown',
+	};
+
+	const class4 = {
+		backgroundColor: 'orange',
+		fontWeight: 'bold',
+		color: 'white',
+	};
+
+	return (
+		<div style={class1}>
+			<p style={class2}>text</p>
+			<p style={class3}>text</p>
+			<p style={class4}>text</p>
+		</div>
+	);
 }
 export default App;
