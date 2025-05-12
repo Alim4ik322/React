54ece383a98f9aeeb13b95f5f73013583c18c520
Alim4ik322 <assanovali23@gmail.com>
2025-05-12 20:44:45 +0300

    82

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,28 +6,44 @@ import uuid from 'react-uuid';
 import User from './User';
 import Employee from './Employee';
 function App() {
+	const lastName1 = 'Кузнецов';
+	const firstName1 = 'Алексей';
+	const middleName1 = 'Викторович';
+	const salary1 = '70000';
+
+	const lastName2 = 'Смирнова';
+	const firstName2 = 'Мария';
+	const middleName2 = 'Павловна';
+	const salary2 = '80000';
+
+	const lastName3 = 'Тарасов';
+	const firstName3 = 'Дмитрий';
+	const middleName3 = 'Алексеевич';
+	const salary3 = '75000';
+
 	return (
 		<div>
 			<Employee
-				lastName="Иванов"
-				firstName="Иван"
-				middleName="Иванович"
-				salary="50000"
+				lastName={lastName1}
+				firstName={firstName1}
+				middleName={middleName1}
+				salary={salary1}
 			/>
 			<Employee
-				lastName="Петров"
-				firstName="Пётр"
-				middleName="Петрович"
-				salary="60000"
+				lastName={lastName2}
+				firstName={firstName2}
+				middleName={middleName2}
+				salary={salary2}
 			/>
 			<Employee
-				lastName="Сидоров"
-				firstName="Сидор"
-				middleName="Сидорович"
-				salary="55000"
+				lastName={lastName3}
+				firstName={firstName3}
+				middleName={middleName3}
+				salary={salary3}
 			/>
 		</div>
 	);
 }
+
 
 export default App;
