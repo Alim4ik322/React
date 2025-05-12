a870aed878e24448283812c9462976380edff566
Alim4ik322 <assanovali23@gmail.com>
2025-05-12 13:28:22 +0300

    59

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,23 +3,33 @@ import './App.css';
 import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import uuid from 'react-uuid';
-function tocel(num) {
-	return ((num-32)*5)/9;
-}
+// ❌ Удалено: import { useState } from "react";
+
 
 function App() {
-	const [value, setValue] = useState(0);
-	
-	function handleChange(event) {
-		setValue(event.target.value);
-	}
-	
-	return <div>
-		<input value={value} onChange={handleChange} 
-			/> 
-		<p>{tocel(value)}</p>
-	</div>;
+  const [values, setValues] = useState(Array(5).fill(0));
+
+  function handleChange(index, event) {
+    const newValues = [...values];
+    newValues[index] = +event.target.value;
+    setValues(newValues);
+  }
+
+  const average = values.reduce((sum, num) => sum + num, 0) / values.length;
+
+  return (
+    <div>
+      {values.map((value, index) => (
+        <input
+          key={index}
+          value={value}
+          onChange={(event) => handleChange(index, event)}
+          type="number"
+        />
+      ))}
+      <p>Среднее арифметическое: {average}</p>
+    </div>
+  );
 }
 
 export default App;
-  
