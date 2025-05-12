1f0701d6eaf83f44ce7ea7dcb104cfa83d687171
Alim4ik322 <assanovali23@gmail.com>
2025-05-12 17:41:00 +0300

    63.2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,45 +5,25 @@ import { nanoid } from 'nanoid';
 import uuid from 'react-uuid';
 // ❌ Удалено: import { useState } from "react";
 
+
 function App() {
-	const [html, setHtml] = useState(false);
-	const [css, setCss] = useState(false);
-	const [js, setJs] = useState(false);
+	const [visible, setVisible] = useState(false);
 
 	return (
 		<div>
 			<label>
 				<input
 					type="checkbox"
-					checked={html}
-					onChange={() => setHtml(!html)}
+					checked={visible}
+					onChange={() => setVisible(!visible)}
 				/>
-				HTML
+				Показать абзац
 			</label>
-			<br />
-			<label>
-				<input
-					type="checkbox"
-					checked={css}
-					onChange={() => setCss(!css)}
-				/>
-				CSS
-			</label>
-			<br />
-			<label>
-				<input
-					type="checkbox"
-					checked={js}
-					onChange={() => setJs(!js)}
-				/>
-				JS
-			</label>
-			<br />
-			<p>HTML: {html ? 'знает' : 'не знает'}</p>
-			<p>CSS: {css ? 'знает' : 'не знает'}</p>
-			<p>JS: {js ? 'знает' : 'не знает'}</p>
+
+			{visible && <p>Это абзац, который можно показать или скрыть</p>}
 		</div>
 	);
 }
+
 
 export default App;
