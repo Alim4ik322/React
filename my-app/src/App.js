c80648e70774292ab22bf9af977f30abb1c99db6
Alim4ik322 <assanovali23@gmail.com>
2025-05-12 23:51:38 +0300

    95.2.2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,52 +8,51 @@ import Employee from './Employee';
 import Users from './Users';
 import { nanoid as id } from 'nanoid';
 
-function TemperatureInput({ scale, temperature, onTemperatureChange }) {
-  const scaleNames = { c: 'Цельсий', f: 'Фаренгейт' };
-
-  return (
-    <fieldset>
-      <legend>Температура в {scaleNames[scale]}:</legend>
-      <input
-        type="number"
-        value={temperature}
-        onChange={e => onTemperatureChange(e.target.value)}
-      />
-    </fieldset>
-  );
-}
-
-function toCelsius(f) {
-  return ((f - 32) * 5) / 9;
-}
-
-function toFahrenheit(c) {
-  return (c * 9) / 5 + 32;
-}
-
-function Calculator() {
-  const [temperature, setTemperature] = useState('');
-  const [scale, setScale] = useState('c');
-
-  const handleCelsiusChange = (temp) => {
-    setScale('c');
-    setTemperature(temp);
-  };
-
-  const handleFahrenheitChange = (temp) => {
-    setScale('f');
-    setTemperature(temp);
-  };
-
-  const celsius = scale === 'f' ? toCelsius(temperature) : temperature;
-  const fahrenheit = scale === 'c' ? toFahrenheit(temperature) : temperature;
-
+function CheckboxList({ items, checkedItems, onChange }) {
   return (
     <div>
-      <TemperatureInput scale="c" temperature={celsius} onTemperatureChange={handleCelsiusChange} />
-      <TemperatureInput scale="f" temperature={fahrenheit} onTemperatureChange={handleFahrenheitChange} />
+      {items.map((item, index) => (
+        <label key={item}>
+          <input
+            type="checkbox"
+            checked={checkedItems[index]}
+            onChange={() => {
+              const newChecked = [...checkedItems];
+              newChecked[index] = !newChecked[index];
+              onChange(newChecked);
+            }}
+          />
+          {item}
+        </label>
+      ))}
     </div>
   );
 }
 
-export default Calculator;
+function SelectAll() {
+  const items = ['яблоко', 'банан', 'вишня'];
+  const [checkedItems, setCheckedItems] = useState([false, false, false]);
+
+  const allSelected = checkedItems.every(Boolean);
+
+  const toggleAll = () => {
+    const newValue = !allSelected;
+    setCheckedItems(Array(items.length).fill(newValue));
+  };
+
+  return (
+    <div>
+      <label>
+        <input type="checkbox" checked={allSelected} onChange={toggleAll} />
+        Выбрать всё
+      </label>
+      <CheckboxList
+        items={items}
+        checkedItems={checkedItems}
+        onChange={setCheckedItems}
+      />
+    </div>
+  );
+}
+
+export default SelectAll;
