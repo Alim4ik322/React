fb26a3d538f03200508fa49e3779da89d88aef23
Alim4ik322 <assanovali23@gmail.com>
2025-05-12 16:47:07 +0300

    62.1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,30 +6,24 @@ import uuid from 'react-uuid';
 // ❌ Удалено: import { useState } from "react";
 
 function App() {
-    const [value, setValue] = useState('');
-    const [sum, setSum] = useState(0);
+	const [checked, setChecked] = useState(false);
+	const [message, setMessage] = useState('');
 
-    function handleChange(event) {
-        const newValue = event.target.value;
-        setValue(newValue);
+	function handleClick() {
+		setMessage(checked ? 'Привет, пользователь!' : 'До свидания!');
+	}
 
-        const numbers = newValue.split(/\r?\n/)
-            .map(line => line.trim())
-            .filter(line => line !== '')
-            .map(Number);
-
-        console.log("Исходный ввод:", newValue);
-        console.log("Массив чисел:", numbers);
-        console.log("Рассчитанная сумма:", numbers.reduce((acc, num) => acc + num, 0));
-
-        setSum(numbers.reduce((acc, num) => acc + num, 0));
-    }
-
-    return (
-        <div>
-            <textarea value={value} onChange={handleChange} />
-            <p>Сумма: {sum}</p>
-        </div>
-    );
+	return (
+		<div>
+			<input
+				type="checkbox"
+				checked={checked}
+				onChange={() => setChecked(!checked)}
+			/>
+			<button onClick={handleClick}>Проверить</button>
+			<p>{message}</p>
+		</div>
+	);
 }
+
 export default App;
